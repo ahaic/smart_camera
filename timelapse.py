9b77f4ec6abb517859b933880646e17c259fd5d4
ahaic <ahaic@users.noreply.github.com>
2019-07-10 22:33:40 +0800

    update
    covert to class  instance

--- timelapse.py
+++ timelapse.py
@@ -1,11 +1,12 @@
 # works on python 3.6 Mac
-
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+# latest update : 10 July 2019  22:31
 
 import cv2,logging
 from time import gmtime, strftime, time,sleep,strftime
 import configparser
 import os
-
 
 logging.basicConfig(format='%(asctime)s %(message)s',
                         filename=os.getcwd()+'camera.log',
@@ -13,74 +14,59 @@ logging.basicConfig(format='%(asctime)s %(message)s',
 logger = logging.getLogger(__name__)
 
 
+class camera():
+    def __init__(self,id):
+    # load configuration file and return the camer link
+    # id = camera id
+        config = configparser.ConfigParser()
+        conf_file = "camera.conf"
+        print("- Load config file")
+        config.read(conf_file)
+        interval = config['settings']['interval']
+        frames = config['settings']['frames']
+        link = config['Camera_2']['link']
+        dir = config['settings']['dir']
 
-def load_config():
+        self.interval = interval
+        self.frames = frames
+        self.link = link
+        self.dir = dir
+        self.id=id
 
-# load configuration file and return the camer link
-    config = configparser.ConfigParser()
-    conf_file = "camera.conf"
-    print("- Load config file")
-    config.read(conf_file)
-    interval = config['settings']['interval']
-    frames = config['settings']['frames']
-    link = config['Camera_2']['link']
-    dir = config['settings']['dir']
-    if bool(dir):
-        return interval,frames,link,dir
-    else:
-        dir = os.getcwd()
-        return interval,frames,link,dir
+    def connection(self):
+        try:
+            #interval,frames,link=load_config()
+            #print(self.link)
+            cap = cv2.VideoCapture(self.link)
+    #       print(cap.isOpened())
+            while (cv2.VideoCapture.isOpened(cap)):
+                #print("camera connection establised")
+
+                ret, image = cap.read()
+                #del(cap)
+                return ret,image
+            else:
+                logger.error('error connection....')
+        except:
+            logger.debug('exception error')
 
 
-    #print(interval,frames,link,dir)
-
-def connection(link):
-    try:
-        #interval,frames,link=load_config()
-
-        cap = cv2.VideoCapture(link)
-#        print(cap.isOpened())
-
-        while (cv2.VideoCapture.isOpened(cap)):
-            #print("camera connection establised")
-
-            ret, image = cap.read()
-            #del(cap)
-            return ret,image
+    def capture(self):
+        ret,image=self.connection()
+        if (ret==True):
+            cv2.imwrite(os.path.join(self.dir,strftime("%Y-%m-%d %H-%M-%S")+'.jpg'), image)
         else:
-            logger.error('error connection....')
-    except:
-        logger.debug('exception error')
-
-
-def capture(link,dir):
-    ret,image=connection(link)
-    if (ret==True):
-        cv2.imwrite(os.path.join(dir,strftime("%Y-%m-%d %H-%M-%S")+'.jpg'), image)
-    else:
-        #print('release camera and capture again')
-        logger.debug('release camera and capture again')
-
-def timelapse_video():
-    pass
-
-
-def photo_upload():
-    pass
-
-
-def face_recognition():
-    pass
-
+            #print('release camera and capture again')
+            logger.debug('release camera and capture again')
 
 
 if __name__ == '__main__':
     print('initializing.....')
-    interval,frames,link,dir=load_config()
-    print('reading camera from ',link)
-    print('save files in ',dir)
+    x= camera(2) 
+    print('reading camera position from ',x.id)
+    print('save files in ',x.dir)
 
-    for i in range(int(frames)):
-        capture(link,dir)
+    for i in range(int(x.frames)):
+        x.capture()
         print('this is' ,i ,'@', strftime("%Y-%m-%d-%H:%M:%S"),'photos')
-        sleep(int(interval))
+        sleep(int(x.interval))
